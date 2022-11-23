91fbb21855653630580441f497855f6a111bbd2d
František Navrkal <F.Navrkal@gmail.com>
2022-11-23 20:42:55 +0000

    api code generation script fix

--- frontend/generate.sh
+++ frontend/generate.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
-
-rm -f /tmp/openapi.json
-wget http://localhost:8000/openapi.json
-npx openapi -i /tmp/openapi.json -o src/generated/api
-rm /tmp/openapi.json
+tmp_dir=`mktemp -d`
+json="$tmp_dir/openapi.json"
+wget http://localhost:8000/openapi.json -O $json
+npx openapi -i $json -o src/generated/api
+rm -rf $tmp_dir
