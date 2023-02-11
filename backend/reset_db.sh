c5f5ee02723826f113a165094c85fd0b68141ab6
Ada Bohm <ada@kreatrix.org>
2023-02-11 16:32:42 +0100

    reset_db fixed

--- backend/reset_db.sh
+++ backend/reset_db.sh
@@ -1,3 +1,11 @@
+export VFF_RUN_ENV=local
+export VFF_INVITATION_CODES=test1 test2
+export DB_HOST=db
+export DB_USER=postgres
+export DB_PASSWORD=postgres
+export DB_NAME=voicedb
+
+
 export PGPASSWORD=postgres
 psql -U postgres -h db -c "DROP DATABASE voicedb;"
 python3 -m app initdb --create-test-user
